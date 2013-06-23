28a3030c7512f78faa5224661d611faf2144373f
Anders Lundberg <dan.anders.lundberg@server.fake>
2013-06-23 12:36:59 +0200

    Moved init to model as workaround for prerenderview problem

--- google-app/recipe-web/src/main/java/se/dandel/recipe/web/RecipesController.java
+++ google-app/recipe-web/src/main/java/se/dandel/recipe/web/RecipesController.java
@@ -1,15 +1,9 @@
 package se.dandel.recipe.web;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-
-import se.dandel.recipe.Recipe;
-import se.dandel.recipe.RecipeId;
-import se.dandel.recipe.RecipeType;
 
 @ManagedBean
 public class RecipesController implements Serializable {
@@ -18,25 +12,6 @@ public class RecipesController implements Serializable {
 
     @ManagedProperty(value = "#{recipesModel}")
     private RecipesModel recipesModel;
-
-    public void load() {
-        System.out.println("qwerqwer");
-        List<RecipeItem> items = new ArrayList<>();
-        items.add(createItem(RecipeType.MEAT, "BIFF_STROGANOFF", "Biff Stroganoff"));
-        items.add(createItem(RecipeType.ITRIM_LCD, "ITRIM_LCD_CHOCOLATE", "Itrim LCD Choklad"));
-        items.add(createItem(RecipeType.FISH, "FISK_GRYTA_SAFFRAN_RAKOR", "Lyxig fiskgryta med saffran och räkor"));
-        recipesModel.setItems(items);
-    }
-
-    private RecipeItem createItem(RecipeType type, String id, String name) {
-        RecipeItem item = new RecipeItem();
-        Recipe recipe = new Recipe();
-        recipe.setType(type);
-        recipe.setId(new RecipeId(id));
-        recipe.setName(name);
-        item.setRecipe(recipe);
-        return item;
-    }
 
     public void setRecipesModel(RecipesModel recipesModel) {
         this.recipesModel = recipesModel;
--- google-app/recipe-web/src/main/java/se/dandel/recipe/web/RecipesModel.java
+++ google-app/recipe-web/src/main/java/se/dandel/recipe/web/RecipesModel.java
@@ -1,11 +1,17 @@
 package se.dandel.recipe.web;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import se.dandel.recipe.Recipe;
+import se.dandel.recipe.RecipeId;
+import se.dandel.recipe.RecipeType;
 
 @ManagedBean
 @ViewScoped
@@ -16,13 +22,28 @@ public class RecipesModel implements Serializable {
 
     private List<RecipeItem> items;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("qwerqwer");
+        items = new ArrayList<>();
+        items.add(createItem(RecipeType.MEAT, "BIFF_STROGANOFF", "Biff Stroganoff"));
+        items.add(createItem(RecipeType.ITRIM_LCD, "ITRIM_LCD_CHOCOLATE", "Itrim LCD Choklad"));
+        items.add(createItem(RecipeType.FISH, "FISK_GRYTA_SAFFRAN_RAKOR", "Lyxig fiskgryta med saffran och räkor"));
+    }
+
+    private RecipeItem createItem(RecipeType type, String id, String name) {
+        RecipeItem item = new RecipeItem();
+        Recipe recipe = new Recipe();
+        recipe.setType(type);
+        recipe.setId(new RecipeId(id));
+        recipe.setName(name);
+        item.setRecipe(recipe);
+        return item;
+    }
+
     public List<RecipeItem> getItems() {
         logger.info("getItems");
         return items;
-    }
-
-    void setItems(List<RecipeItem> items) {
-        this.items = items;
     }
 
 }
