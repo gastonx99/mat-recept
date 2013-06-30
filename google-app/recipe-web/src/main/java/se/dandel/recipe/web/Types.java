a9668baa515139e2655c4fa7900ba80f81d8e373
Anders Lundberg <dan.anders.lundberg@server.fake>
2013-06-30 13:15:40 +0200

    dos2unix: to dos

--- google-app/recipe-web/src/main/java/se/dandel/recipe/web/Types.java
+++ google-app/recipe-web/src/main/java/se/dandel/recipe/web/Types.java
@@ -1,22 +1,22 @@
-package se.dandel.recipe.web;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Types {
-    private static Map<String, String> TYPES = new HashMap<>();
-    static {
-        TYPES.put("FISH", "Fisk");
-        TYPES.put("ITRIM_LCD", "Itrim LCD");
-        TYPES.put("MEAT", "Kött");
-        TYPES.put("POULTRY", "Fågel");
-        TYPES.put("SIDE_ORDER_GREEN", "Grönsaker");
-        TYPES.put("SIDE_ORDER_ROOT", "Rotfrukter");
-        TYPES.put("VEGGIE", "Vegetariskt");
-    }
-
-    public static String get(String key) {
-        return TYPES.get(key);
-    }
-
-}
+package se.dandel.recipe.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Types {
+    private static Map<String, String> TYPES = new HashMap<>();
+    static {
+        TYPES.put("FISH", "Fisk");
+        TYPES.put("ITRIM_LCD", "Itrim LCD");
+        TYPES.put("MEAT", "Kött");
+        TYPES.put("POULTRY", "Fågel");
+        TYPES.put("SIDE_ORDER_GREEN", "Grönsaker");
+        TYPES.put("SIDE_ORDER_ROOT", "Rotfrukter");
+        TYPES.put("VEGGIE", "Vegetariskt");
+    }
+
+    public static String get(String key) {
+        return TYPES.get(key);
+    }
+
+}
